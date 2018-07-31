98916d54c7599e89cad1613d0fb81f6a622e97e0
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-31 19:29:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is bob"
+  end 
+   get '/hometown' do
+    "My hometown is Buffalo"
+  end 
+    get '/favorite-song' do
+    "My favorite song is shake it off"
+  end 
 end
